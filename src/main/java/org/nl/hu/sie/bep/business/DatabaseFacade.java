415eed2343322a21dec5bb525d39638878562e5b
dmytroshelomov <dmytro.shelomov@student.hu.nl>
2019-06-23 20:02:47 +0000

    Add files via upload
    ADDING DATABASE FACADE CLASS

---
+++ src/main/java/org/nl/hu/sie/bep/business/DatabaseFacade.java
@@ -0,0 +1,19 @@
+package org.nl.hu.sie.bep.Loader;
+
+import org.nl.hu.sie.bep.Loader.models.Data;
+import org.nl.hu.sie.bep.Loader.models.KlantMysqlImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class DatabaseFacade {
+    private final KlantMysqlImpl mysql;
+
+    public DatabaseFacade() {
+        this.mysql = new KlantMysqlImpl();
+    }
+
+    public List<Data> get(int month) throws SQLException {
+        return this.mysql.get(month);
+    }
+}
