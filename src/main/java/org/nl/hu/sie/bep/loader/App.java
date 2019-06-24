306751104761105e7d77be6b310e3e2aa7db04ba
Yui Mizuno <dmytro.shelomov@student.hu.nl>
2019-06-24 23:16:02 +0200

    Default Changelist

--- src/main/java/org/nl/hu/sie/bep/loader/App.java
+++ src/main/java/org/nl/hu/sie/bep/loader/App.java
@@ -2,10 +2,7 @@ package org.nl.hu.sie.bep.loader;
 
 import org.nl.hu.sie.bep.loader.models.Data;
 import org.nl.hu.sie.bep.loader.models.DatabaseInformationRetriever;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -13,11 +10,8 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) {
-        try {
-            connect();
-        } finally {
 
-        }
+            connect();
     }
 
     private static void connect()  {
